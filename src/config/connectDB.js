988a6e8b504c86fada53b8254a7a7ecaabae1a24
doankhoa126 <doankhoa126@gmail.com>
2024-06-25 01:06:43 +0700

    update merger fe and be to main

--- src/config/connectDB.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
-// const supabaseUrl = 'https://asplynrorebnsajukbnu.supabase.co'
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzcGx5bnJvcmVibnNhanVrYm51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg3MTM1NDMsImV4cCI6MjAzNDI4OTU0M30.MFbYnzw5WFsIZ6oKdSkD9ySF5R6j9rrCWJ_Mst5pfy4'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase;
--- src/route/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getProducts, getProductDetails } from '../controller/product.js';
-
-const router = express.Router();
-
-router.get('/getAllProducts', getProducts)
-router.get('/getProductDetails/:id', getProductDetails)
-
-export default router;
